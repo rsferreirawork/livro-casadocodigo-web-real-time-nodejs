6b243e6825420f0b262e3bf250b9c2c1f9971736
Ramon Ferreira <ramon.ferreira@maxifrota.com.br>
2022-02-16 08:16:23 -0300

    Add libs cookie-parser express-session body-parser

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const path = require('path');
 const consign = require('consign');
+const cookieParser = require('cookie-parser');
+const expressSession = require('express-session');
+const bodyParser = require('body-parser');
 const app = express();
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.use(cookieParser('ntalk'));
+app.use(expressSession());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(path.join(__dirname, 'public')));
+
 
 consign({locale: 'pt-br'})
 	.include('models')
