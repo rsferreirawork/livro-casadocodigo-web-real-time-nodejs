56ccca5c07766d0df97d76ec2a95c8701e2d2871
Ramon Ferreira <ramon.ferreira@maxifrota.com.br>
2022-02-16 08:17:58 -0300

    Add contatos initial functionality

---
+++ controllers/contatos.js
@@ -0,0 +1,10 @@
+module.exports = (app) => {
+    const ContatosController = {
+        index(req, res){
+            const {usuario} = req.session;
+            res.render('contatos/index', usuario);
+        }
+    };
+    
+    return ContatosController;
+};
--- controllers/home.js
+++ controllers/home.js
@@ -2,6 +2,24 @@ module.exports = (app) => {
   const HomeController = {
     index(req, res) {
       res.render('home/index');
+    },
+
+    login(req, res){
+      const {usuario} = req.body;
+      const {nome, email} = usuario;
+
+      if(email && nome){
+        usuario.contatos = [];
+        req.session.usuario = usuario;
+        res.redirect('/contatos');
+      }else{
+        res.redirect('/');
+      }
+    },
+
+    logout(req, res){
+      req.session.destroy();
+      res.redirect('/')
     }
   };
   return HomeController;
